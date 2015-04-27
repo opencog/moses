20042b479faeda83f3712032f53e4162af482c2e
Linas Vepstas <linasvepstas@gmail.com>
2015-04-27 13:51:08 -0500

    fix scripts to find cogutil

--- lib/FindCogUtil.cmake
+++ lib/FindCogUtil.cmake
@@ -1,11 +1,13 @@
-find_library(COGUTIL_LIBRARY   NAMES cogutil  PATH /usr/lib /usr/lib64)
+find_library(COGUTIL_LIBRARY
+	NAMES cogutil
+	PATH /usr/lib /usr/lib64 /usr/local/lib /usr/local/lib64)
 
 INCLUDE (CheckIncludeFiles)
 
 # The location of iberty.h varies according to the distro.
 # Fine the right location.
 find_path(
-    COGUTIL_INCLUDE_DIR Logger.h
+    COGUTIL_INCLUDE_DIR backtrace-symbols.h
     PATHS
         /usr/include
         /usr/local/include
@@ -14,7 +16,7 @@ find_path(
 )
 
 set(CMAKE_REQUIRED_INCLUDES ${COGUTIL_INCLUDE_DIR})
-CHECK_INCLUDE_FILES (Loggery.h HAVE_COGUTIL_H)
+CHECK_INCLUDE_FILES (backtrace-symbols.h HAVE_COGUTIL_H)
 
 if (COGUTIL_LIBRARY AND HAVE_COGUTIL_H)
 	set( COGUTIL_FOUND TRUE )
@@ -23,5 +25,5 @@ endif (COGUTIL_LIBRARY AND HAVE_COGUTIL_H)
 if ( COGUTIL_FOUND )
 	message( STATUS "Found CogUtil: ${COGUTIL_LIBRARY}")
 else ( COGUTIL_FOUND )
-	message( STATUS "CigUtil not found")
+	message( STATUS "CogUtil not found")
 endif ( COGUTIL_FOUND )
