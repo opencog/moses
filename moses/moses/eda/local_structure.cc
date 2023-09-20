a535d33e2314ffc766ac82956ca162b64dcce190
Linas Vepstas <linasvepstas@gmail.com>
2023-09-19 19:42:37 -0500

    Cleanup boost::bind

--- moses/moses/eda/local_structure.cc
+++ moses/moses/eda/local_structure.cc
@@ -39,11 +39,10 @@ namespace moses {
 // instances [l, u)
 bool local_structure_model::is_uniform_on(iptr_iter l, iptr_iter u, int idx) const
 {
-    using namespace boost::placeholders;
     return (adjacent_find(boost::make_indirect_iterator(l), boost::make_indirect_iterator(u),
-                          bind(std::not_equal_to<disc_t>(),
-                               bind(&field_set::get_raw, &_fields, _1, idx),
-                               bind(&field_set::get_raw, &_fields, _2, idx))) ==
+                          std::bind(std::not_equal_to<disc_t>(),
+                               std::bind(&field_set::get_raw, &_fields, std::placeholders::_1, idx),
+                               std::bind(&field_set::get_raw, &_fields, std::placeholders::_2, idx))) ==
             boost::make_indirect_iterator(u));
 }
 
@@ -52,8 +51,6 @@ void local_structure_model::rec_split_term(iptr_iter l, iptr_iter u,
                                            dtree::iterator node,
                                            term_tree::iterator osrc)
 {
-    using namespace boost::placeholders;
-
     int raw_arity = osrc.number_of_children() + 1;
 
     if (is_uniform_on(l, u, idx) || raw_arity == 1) {
@@ -71,8 +68,8 @@ void local_structure_model::rec_split_term(iptr_iter l, iptr_iter u,
         vector<iptr_iter> pivots(raw_arity);
         pivots.back() = u;
         n_way_partition(l, u,
-                        bind(&field_set::get_raw, &_fields,
-                             bind(valueof<const instance>, _1), src_idx),
+                        std::bind(&field_set::get_raw, &_fields,
+                             std::bind(valueof<const instance>, std::placeholders::_1), src_idx),
                         raw_arity, pivots.begin());
 
         // we don't recurse on the leftmost partition - it's term_spec::Stop
@@ -80,8 +77,8 @@ void local_structure_model::rec_split_term(iptr_iter l, iptr_iter u,
         for_each(pivots.begin(), --pivots.end(), ++pivots.begin(),
                  make_counting_iterator(++node.begin()),
                  make_counting_iterator(osrc.begin()),
-                 bind(&local_structure_model::rec_split_term, this, _1, _2,
-                      src_idx + 1, idx, _3, _4));
+                 std::bind(&local_structure_model::rec_split_term, this, std::placeholders::_1, std::placeholders::_2,
+                      src_idx + 1, idx, std::placeholders::_3, std::placeholders::_4));
     } else {
         //we're the parent of leaves
         dtree::sibling_iterator dsib = ++node.begin();
@@ -96,9 +93,7 @@ void local_structure_model::rec_split_contin(iptr_iter l, iptr_iter u,
                                              int src_idx, int idx,
                                              dtree::iterator node)
 {
-    using namespace boost::placeholders;
-
-    if(is_uniform_on(l, u, idx))
+    if (is_uniform_on(l, u, idx))
         return;
 
     split(src_idx, idx, node);
@@ -106,8 +101,8 @@ void local_structure_model::rec_split_contin(iptr_iter l, iptr_iter u,
     if (src_idx < idx - 1) { // important - needs to match contin_spec::Left/Right
         vector<iptr_iter> pivots(2);
         n_way_partition(l, u,
-                        bind(&field_set::get_raw, &_fields,
-                             bind(valueof<const instance>, _1), src_idx),
+                        std::bind(&field_set::get_raw, &_fields,
+                             std::bind(valueof<const instance>, std::placeholders::_1), src_idx),
                         3, pivots.begin());
         rec_split_contin(pivots[0], pivots[1], src_idx + 1, idx, ++node.begin());
         rec_split_contin(pivots[1], u, src_idx + 1, idx, node.last_child());
